41d60754224bb2409e979fff5e6f9a38bfc0542b
Gautam <gautamssingh99@gmail.com>
2022-07-28 16:10:02 +0530

    lates

--- Reports/report.js
+++ Reports/report.js
@@ -1,48 +1,15 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "8a992b16-fb7f-421d-bfcc-2e508c009cef",
-    "feature": "Get products categories from above tab",
-    "scenario": "Get categories of products related to sports from above tab",
-    "start": 1658999003712,
-    "group": 16,
-    "content": "",
-    "tags": "",
-    "end": 1658999022061,
-    "className": "passed"
-  },
-  {
-    "id": "93592f44-3f91-44db-92a2-caff18e190e3",
+    "id": "649ef848-8dd1-49ae-aec3-690990aa7ad3",
     "feature": "Get all adds coming on the top of screen",
     "scenario": "Get the adds coming on the of screen",
-    "start": 1658998965761,
+    "start": 1659004544158,
     "group": 16,
     "content": "",
     "tags": "",
-    "end": 1658999003696,
-    "className": "passed"
-  },
-  {
-    "id": "d5e4faab-6a23-456e-96b0-3399b4e40ce1",
-    "feature": "Get most popular and top brands from All Categories",
-    "scenario": "Get most popular and top brands Mobiles and Electronics from All Categories",
-    "start": 1658998921974,
-    "group": 16,
-    "content": "",
-    "tags": "",
-    "end": 1658998943219,
-    "className": "passed"
-  },
-  {
-    "id": "7d141146-c20f-45db-afe5-a4cea078402c",
-    "feature": "Get most popular and top brands from All Categories",
-    "scenario": "Get most popular and top brands Mahali from All Categories",
-    "start": 1658998943274,
-    "group": 16,
-    "content": "",
-    "tags": "",
-    "end": 1658998965744,
-    "className": "passed"
+    "end": 1659004573004,
+    "className": "failed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
